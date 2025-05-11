e0965926fc830ec7dc6f61b206f82798573826d5
zyn <96938079+zyn1014@users.noreply.github.com>
2025-05-11 17:34:27 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-echo '<script>alert("Successfully Logged Out!"); window.location.href="../BASED/Login/login.php";</script>';
+echo '<script>localStorage.removeItem("selectedItem");alert("Successfully Logged Out!"); window.location.href="../BASED/Login/login.php";</script>';
 ?>
